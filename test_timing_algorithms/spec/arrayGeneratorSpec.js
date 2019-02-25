bafd1a03368692acc7029fbfd2f7f82aa9a36fa1
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-25 17:21:37 +0000

    added arrayGenerator test

--- test_timing_algorithms/spec/arrayGeneratorSpec.js
+++ test_timing_algorithms/spec/arrayGeneratorSpec.js
@@ -1,6 +1,10 @@
 describe('generateArray', function() {
-  it('should generate an array', function() {
-    generateArray();
+  it('should generate an array with 50000 objects', function() {
+    generateArray(50000);
     expect(arr.length).toEqual(50000);
+  })
+  it('should generate an array with 0 objects', function() {
+    generateArray(0);
+    expect(arr.length).toEqual(0);
   })
 })
