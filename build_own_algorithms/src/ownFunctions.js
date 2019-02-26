7296a03baa0e5c9736f5de00b3271e677e3c7da3
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-26 14:00:02 +0000

    created a folder for my own self built functions. implemented jasmine

---
+++ build_own_algorithms/src/ownFunctions.js
@@ -0,0 +1,31 @@
+function last() {
+
+}
+
+function last2() {
+
+}
+
+function reverse() {
+
+}
+
+function reverse2() {
+
+}
+
+function sort() {
+
+}
+
+function sort2() {
+
+}
+
+function shuffle() {
+
+}
+
+function shuffle2() {
+
+}
