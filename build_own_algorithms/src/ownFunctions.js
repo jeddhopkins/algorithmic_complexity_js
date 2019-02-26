d6c15b6979e7f269523b21800741600e1407f1f7
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-26 16:28:32 +0000

    added duplicates function and tests

--- build_own_algorithms/src/ownFunctions.js
+++ build_own_algorithms/src/ownFunctions.js
@@ -1,33 +1,68 @@
 var newArr = [];
 
 function last(arr) {
-  return arr.splice(-1)[0];
+  return arr.pop()
 }
 
 function last2(arr) {
+  return arr.splice(-1)[0];
+}
+
+function last3(arr) {
   return arr[arr.length - 1];
 }
 
-function reverse() {
+
+function reverse(arr) {
+ return arr.reverse()
+}
+
+function reverse2(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+function duplicates(array) {
+  array.sort()
+  result = []
+  while ( array.length != 0 ) {
+    var count = 1
+    var i = 0
+    var j = 1
+    while (array[i] === array[j]) {
+      count += 1
+      j += 1
+    }
+    for (var i = 0; i < count; i++) {
+      if ( count > 1 ) { result.push(array[i]) }
+    }
+    array.splice(0, count)
+  }
+  return result
+}
+
+function reverse3(arr) {
 
 }
 
-function reverse2() {
+function reverse4(arr) {
 
 }
 
-function sort() {
+function sort(arr) {
 
 }
 
-function sort2() {
+function sort2(arr) {
 
 }
 
-function shuffle() {
+function shuffle(arr) {
 
 }
 
-function shuffle2() {
+function shuffle2(arr) {
 
 }
